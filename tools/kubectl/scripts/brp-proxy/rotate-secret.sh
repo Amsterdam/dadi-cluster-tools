bef8da4b94d664c7b61f3092c313b5cd13aa2b3e
Stefan van Beusekom <stefan.van.beusekom@gmail.com>
2025-10-08 15:55:15 +0200

    fix: added concatted hash

--- tools/kubectl/scripts/brp-proxy/rotate-secret.sh
+++ tools/kubectl/scripts/brp-proxy/rotate-secret.sh
@@ -8,17 +8,26 @@ SECONDARY_DATE_HASH=$(kubectl get --ignore-not-found secret brp-hashes -o jsonpa
 if [[ -z $PRIMARY_DATE_HASH ]] ; then
     if [[ -z $SECONDARY_DATE_HASH ]] ; then
         echo "No hashes exist for today or yesterday, so new hashes will be created."
-        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
+        CONCAT_HASH="$SECONDARY_HASH,$PRIMARY_HASH"
+        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_HASH --from-literal=brp_hash_array=$CONCAT_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
     else
         echo "No hash exists for today, only for yesterday. A new hash will be generated for today. Only yesterdays hash will be kept alongside todays hash."
-        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_DATE_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
+        CONCAT_HASH="$SECONDARY_DATE_HASH,$PRIMARY_HASH"
+        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_DATE_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_HASH --from-literal=brp_hash_array=$CONCAT_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
     fi
 else
     if [[ -z $SECONDARY_DATE_HASH ]] ; then
         echo "A hash for today exists, but no hash was found for yesterday. For consistency, a hash for the previous day will be created"
-        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_DATE_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
+        CONCAT_HASH="$SECONDARY_HASH,$PRIMARY_DATE_HASH"
+        kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$SECONDARY_HASH --from-literal=$PRIMARY_DATE=$PRIMARY_DATE_HASH --from-literal=brp_hash_array=$CONCAT_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
 
     else
         echo "A hash already exists for today and yesterday. No actions will be taken"
     fi
 fi
+
+CURRENT_PRIMARY_DATE_HASH=$(kubectl get --ignore-not-found secret brp-hashes -o jsonpath="{.data.$PRIMARY_DATE}")
+CURRENT_SECONDARY_DATE_HASH=$(kubectl get --ignore-not-found secret brp-hashes -o jsonpath="{.data.$SECONDARY_DATE}")
+CURRENT_CONCAT_HASH="$CURRENT_SECONDARY_DATE_HASH,$CURRENT_PRIMARY_DATE_HASH"
+
+kubectl create secret generic brp-hashes --from-literal=$SECONDARY_DATE=$CURRENT_SECONDARY_DATE_HASH --from-literal=$PRIMARY_DATE=$CURRENT_PRIMARY_DATE_HASH --from-literal=brp_hash_array=$CURRENT_CONCAT_HASH --dry-run=client -o yaml | kubectl apply --server-side -f -
