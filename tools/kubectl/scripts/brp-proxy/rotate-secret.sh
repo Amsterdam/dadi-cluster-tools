efb843683e2358818aef78111ab1dc3b7fe2c140
Stefan van Beusekom <stefan.van.beusekom@amsterdam.nl>
2025-10-09 11:46:46 +0200

    fix: rename

--- tools/kubectl/scripts/brp-proxy/rotate-secret.sh
+++ tools/kubectl/scripts/brp-proxy/rotate-secret.sh
@@ -9,19 +9,19 @@ if [[ -z $PRIMARY_DATE_KEY ]] ; then
     if [[ -z $SECONDARY_DATE_KEY ]] ; then
         echo "No keys exist for today or yesterday, so new keys will be created."
         CONCAT_KEY="$SECONDARY_KEY,$PRIMARY_KEY"
-        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_KEY --from-literal=brp_hash_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
+        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_KEY --from-literal=brp_key_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
     else
-        echo "No hash exists for today, only for yesterday. A new hash will be generated for today. Only yesterdays hash will be kept alongside todays hash."
+        echo "No key exists for today, only for yesterday. A new key will be generated for today. Only yesterdays key will be kept alongside todays key."
         CONCAT_KEY="$SECONDARY_DATE_KEY,$PRIMARY_KEY"
-        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_DATE_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_KEY --from-literal=brp_hash_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
+        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_DATE_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_KEY --from-literal=brp_key_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
     fi
 else
     if [[ -z $SECONDARY_DATE_KEY ]] ; then
-        echo "A hash for today exists, but no hash was found for yesterday. For consistency, a hash for the previous day will be created"
+        echo "A key for today exists, but no key was found for yesterday. For consistency, a key for the previous day will be created"
         CONCAT_KEY="$SECONDARY_KEY,$PRIMARY_DATE_KEY"
-        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_DATE_KEY --from-literal=brp_hash_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
+        kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$SECONDARY_KEY --from-literal=$PRIMARY_DATE=$PRIMARY_DATE_KEY --from-literal=brp_key_array=$CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
     else
-        echo "A hash already exists for today and yesterday. No actions will be taken"
+        echo "A key already exists for today and yesterday. No actions will be taken"
     fi
 fi
 
@@ -29,4 +29,4 @@ CURRENT_PRIMARY_DATE_KEY=$(kubectl get --ignore-not-found secret brp-keys -o jso
 CURRENT_SECONDARY_DATE_KEY=$(kubectl get --ignore-not-found secret brp-keys -o jsonpath="{.data.$SECONDARY_DATE}")
 CURRENT_CONCAT_KEY="$CURRENT_SECONDARY_DATE_KEY,$CURRENT_PRIMARY_DATE_KEY"
 
-kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$CURRENT_SECONDARY_DATE_KEY --from-literal=$PRIMARY_DATE=$CURRENT_PRIMARY_DATE_KEY --from-literal=brp_hash_array=$CURRENT_CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
+kubectl create secret generic brp-keys --from-literal=$SECONDARY_DATE=$CURRENT_SECONDARY_DATE_KEY --from-literal=$PRIMARY_DATE=$CURRENT_PRIMARY_DATE_KEY --from-literal=brp_key_array=$CURRENT_CONCAT_KEY --dry-run=client -o yaml | kubectl apply --server-side -f -
